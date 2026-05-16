65ebdb88586f7dc4f8e93ca4699c1dc7263b431c
xeltol <112965872+xeltol1@users.noreply.github.com>
2026-05-16 22:30:23 +0100

    settings.py
    Add configuration variables for bot settings

---
+++ settings.py
@@ -0,0 +1,4 @@
+LOGINTOKENDONOTSHAREEVER = # string, bot login token
+CHANNELID = # int, the channel the bot will delete messages from
+GUILDID = # int, the guild in which deletion will take place
+DONOTDELETEFROMID = # int, bot will only allow messages from this user id
