9c906511a7d7ab273225bfbbeadc3561e1c9b372
xeltol <112965872+xeltol1@users.noreply.github.com>
2026-05-16 23:02:30 +0100

    n

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 LOGIN_TOKEN_DO_NOT_SHARE_EVER = # string, bot login token
-CHANNEL_ID= # int, the channel the bot will delete messages from
+CHANNEL_ID = # int, the channel the bot will delete messages from
 GUILD_ID = # int, the guild in which deletion will take place
 DO_NOT_DELETE_FROM_ID = # int, bot will only allow messages from this user id
